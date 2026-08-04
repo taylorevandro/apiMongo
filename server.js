f276356a301c0338efdc0a81f9420c91d72130d7
taylorevandro <evandrotaylor4@gmail.com>
2026-08-04 10:51:41 -0300

    commit com log de teste

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import app from "./src/app.js";
 
 const PORT = process.env.PORT;
 
+console.log("HOST:", process.env.PGHOST);
+console.log("USER:", process.env.PGUSER);
+console.log("DATABASE:", process.env.PGDATABASE);
+
 dotenv.config();
 
 app.listen(PORT, () => {
